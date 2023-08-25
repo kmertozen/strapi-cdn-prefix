65ac32de65968e2ff9b9d8143e2782fd31386dc0
Göksal Çoşar <cosargoksal@gmail.com>
2023-08-25 13:39:38 +0300

    code formatted

--- server/services/service.js
+++ server/services/service.js
@@ -2,41 +2,48 @@
 
 const CDNKEY = process.env.CDN_IMAGES_BASE_PATH || "";
 
-const replaceUploadsWithCdnFindOne = (obj) => {  
-    if (Array.isArray(obj)) {
-      for (let i = 0; i < obj.length; i++) {
-        obj[i] = replaceUploadsWithCdnFindOne(obj[i]);
-      }
-    } else if (typeof obj === "object" && obj !== null) {
-      for (let key in obj) {
-        if (typeof obj[key] === "string" && obj[key].startsWith("/uploads") && key === "url") {
-            obj[key] = obj[key].replace(/\/uploads/g, CDNKEY);
-        } else {
-            obj[key] = replaceUploadsWithCdnFindOne(obj[key]);
-        }
+const replaceUploadsWithCdnFindOne = (obj) => {
+  if (Array.isArray(obj)) {
+    for (let i = 0; i < obj.length; i++) {
+      obj[i] = replaceUploadsWithCdnFindOne(obj[i]);
+    }
+  } else if (typeof obj === "object" && obj !== null) {
+    for (let key in obj) {
+      if (
+        typeof obj[key] === "string" &&
+        obj[key].startsWith("/uploads") &&
+        key === "url"
+      ) {
+        obj[key] = obj[key].replace(/\/uploads/g, CDNKEY);
+      } else {
+        obj[key] = replaceUploadsWithCdnFindOne(obj[key]);
       }
     }
-    return obj;    
-} 
+  }
+  return obj;
+};
 
-const replaceUploadsWithCdnFindMany = (objs) => { 
-  if(objs?.model?.uid == "plugin::upload.file" && objs?.result?.length > 0){
-      for (const obj of objs["result"]) {
-        if (obj.formats) {
-          for (const formatKey in obj.formats) {
-            if (obj.formats.hasOwnProperty(formatKey)) {
-              const formatObj = obj.formats[formatKey];
-              if (formatObj && formatObj.url) {
-                obj.formats[formatKey].url = obj.formats[formatKey].url.replace(/\/uploads/g, CDNKEY)
-              }
+const replaceUploadsWithCdnFindMany = (objs) => {
+  if (objs?.model?.uid == "plugin::upload.file" && objs?.result?.length > 0) {
+    for (const obj of objs["result"]) {
+      if (obj.formats) {
+        for (const formatKey in obj.formats) {
+          if (obj.formats.hasOwnProperty(formatKey)) {
+            const formatObj = obj.formats[formatKey];
+            if (formatObj && formatObj.url) {
+              obj.formats[formatKey].url = obj.formats[formatKey].url.replace(
+                /\/uploads/g,
+                CDNKEY
+              );
             }
           }
-        }else {
-          obj.url= obj.url.replace(/\/uploads/g, CDNKEY)
         }
+      } else {
+        obj.url = obj.url.replace(/\/uploads/g, CDNKEY);
       }
     }
-} 
+  }
+};
 
 module.exports = ({ strapi }) => ({
   getWelcomeMessage() {
@@ -48,6 +55,6 @@ module.exports = ({ strapi }) => ({
   },
 
   replaceUploadsWithCdnFindMany(obj) {
-    return replaceUploadsWithCdnFindMany(obj)
+    return replaceUploadsWithCdnFindMany(obj);
   },
 });
